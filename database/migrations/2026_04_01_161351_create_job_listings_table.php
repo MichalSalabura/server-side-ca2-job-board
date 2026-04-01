0afda91366bbcb90ea8ee23927ea6f33482e6758
MichalS <D00282723@student.dkit.ie>
2026-04-01 17:14:45 +0100

    feat: create job_listings table

---
+++ database/migrations/2026_04_01_161351_create_job_listings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_listings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->string('salary')->nullable();
+            $table->enum('type', ['full-time', 'part-time', 'contract', 'internship']);
+            $table->enum('status', ['open', 'closed'])->default('open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_listings');
+    }
+};
